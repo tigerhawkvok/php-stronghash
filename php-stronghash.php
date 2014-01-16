95ba1f36c45004e1a37b92c23f993e170f782da6
Philip Kahn <tigerhawkvok@gmail.com>
2014-01-15 19:48:15 -0800

    Fix constant declaration

--- php-stronghash.php
+++ php-stronghash.php
@@ -51,13 +51,16 @@
 
 class Stronghash {
 
-  var $default_rounds=10000;
+  private $default_rounds=10000;
+
+  private function getDefaultRounds() {
+    return $this->$default_rounds;
+  }
 
   public function hasher($data,$salt=null,$use=null,$forcesalt=true,$rounds=null)
   {
     //hashes with most secure algorithm and returns the hash used
-    global $default_rounds;
-    $rounds= preg_match("/^([0-9]+)$/",$rounds) ? $default_rounds:$rounds;
+    $rounds= preg_match("/^([0-9]+)$/",$rounds) ? getDefaultRounds():$rounds;
     if(!is_numeric($rounds) || $rounds<1000) $rounds=10000;
     $userset = empty($use) ? false: true;
     $cryptgo=false;
@@ -288,11 +291,10 @@ class Stronghash {
   {
     if(is_array($orig_data))
       {
-        global $default_rounds;
         $refhash=$orig_data;
         $orig_salt=$orig_data['salt'];
         $orig_algo=$orig_data['algo'];
-        $orig_rounds= preg_match("/^([0-9]+)$/",$orig_data['rounds']) ? $orig_data['rounds']:$default_rounds; // natural number
+        $orig_rounds= preg_match("/^([0-9]+)$/",$orig_data['rounds']) ? $orig_data['rounds']:getDefaultRounds(); // natural number
       }
     else $refhash=$this->hasher($orig_data,$orig_salt,$orig_algo,false,$orig_rounds);
     if(strlen($orig_data)!=strlen($hash)) $hash=$this->hasher($hash,$orig_salt,$orig_algo,false,$orig_rounds);
@@ -306,19 +308,6 @@ class Stronghash {
  * Author: havoc AT defuse.ca
  * www: https://defuse.ca/php-pbkdf2.htm
  */
-
-// These constants may be changed without breaking existing hashes.
-  define("PBKDF2_HASH_ALGORITHM", "sha256");
-  define("PBKDF2_ITERATIONS", 1000);
-  define("PBKDF2_SALT_BYTES", 24);
-  define("PBKDF2_HASH_BYTES", 24);
-
-  define("HASH_SECTIONS", 4);
-  define("HASH_ALGORITHM_INDEX", 0);
-  define("HASH_ITERATION_INDEX", 1);
-  define("HASH_SALT_INDEX", 2);
-  define("HASH_PBKDF2_INDEX", 3);
-
 
 // Compares two strings $a and $b in length-constant time.
   private function slow_equals($a, $b)
